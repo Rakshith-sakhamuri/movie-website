54e6fd5f096fe7936d6e4db0bb45dbf01c105365
rakshith <sakhamurirakshith@gmail.com>
2024-03-24 21:58:47 +0530

    Changes made

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cors());
 
 app.get('/', (request, response) => {
   console.log(request);
-  return response.status(234).send('Welcome To MERN Stack Tutorial');
+  return response.status(234).send('Backend Home');
 });
 
 app.use('/books', booksRoute);
